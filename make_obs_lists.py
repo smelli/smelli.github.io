29dfa43ae04d69d0f59d2473fa232bbc037b6c08
Matthew Kirk <mattkirk1992@gmail.com>
2023-05-21 23:18:06 +0200

    Nicer print out
    LIkely to be more useful for generating HTML later

--- make_obs_lists.py
+++ make_obs_lists.py
@@ -7,11 +7,20 @@ gl = smelli.GlobalLikelihood()
 for ll_name, ll in chain(gl.likelihoods.items(), gl.fast_likelihoods.items()):
     print(ll_name)
     print()
-    for obs in ll.observables:
-        match obs:
-            case (name, *b):
-                print(name, "with args", b)
+    for obs_info in ll.observables:
+        # Either a string with the name of the obs, or a tuple whose first 
+        # element is the name, and others are the arguments passed.
+        # For python3.10+, we can use match case since I think it's cool :)
+        match obs_info:
+            case (name, *args):
+                pass
             case name:
-                print(name)
+                args = None
+        o = flavio.Observable[name]
+        text = f"{o.name}"
+        if args:
+            text += " @ "
+            args_info = zip(o.arguments, args)
+            text += ", ".join(f"{name} = {value}" for name, value in args_info)
+        print(text)
     print("-"*20)
-
